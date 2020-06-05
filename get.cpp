f41eab33eb932770c760880a1c8d15ac256ef919
HackHeart-cyber <anxinyu2021@163.com>
2020-06-05 03:30:49 -0700

    Your message about the commit

---
+++ get.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//getline()
+	char buffer [10];
+	cin.getline(buffer, 10);
+	cout << buffer << endl;
+	cin.getline(buffer, 10);
+	cout << buffer << endl;
+
+	//char c;
+	//cin.get(c);
+	//cout.put(c);
+}
